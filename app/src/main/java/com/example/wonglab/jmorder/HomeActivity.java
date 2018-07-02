77cddf5026c59fd6a70c8a05d5403af18ca279d1
unknown <abhinandannabera@yahoo.in>
2018-07-02 12:08:46 +0800

    Build Version 1.0.1

--- app/src/main/java/com/example/wonglab/jmorder/HomeActivity.java
+++ app/src/main/java/com/example/wonglab/jmorder/HomeActivity.java
@@ -1,12 +1,15 @@
 package com.example.wonglab.jmorder;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -33,12 +36,20 @@ import java.io.UnsupportedEncodingException;
 import java.sql.SQLException;
 import java.util.List;
 
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.RuntimePermissions;
+
 import static com.example.wonglab.jmorder.NewOrderActivity.databaseHelper;
 
+@RuntimePermissions
 public class HomeActivity extends AppCompatActivity {
 
     private Dao<Order, String> orderDao;
     private Dao<Item, String> itemDao;
+    private int STORAGE_PERMISSION_CODE = 1;
     Button newOrder, sendOrders, deleteOrders;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,7 +74,7 @@ public class HomeActivity extends AppCompatActivity {
         sendOrders.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                getAllstackOrder();
+                HomeActivityPermissionsDispatcher.readWriteExternalStorageWithPermissionCheck(HomeActivity.this);
             }
         });
 
@@ -74,6 +85,44 @@ public class HomeActivity extends AppCompatActivity {
                 Toast.makeText(HomeActivity.this, "Deleted Order Database!", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @NeedsPermission({Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE})
+    void readWriteExternalStorage(){
+        Toast.makeText(HomeActivity.this, "Done!", Toast.LENGTH_SHORT).show();
+        getAllstackOrder();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        HomeActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+
+    @OnShowRationale({Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE})
+    void showRationalForStorage(final PermissionRequest request){
+        new AlertDialog.Builder(this)
+                .setTitle("Permission Needed")
+                .setMessage("This app requires to access external storage")
+                .setPositiveButton("Allow", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        request.proceed();
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        request.cancel();
+                    }
+                })
+                .create()
+                .show();
+    }
+
+    @OnPermissionDenied({Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE})
+    void onExternalStorageDenied(){
+        Toast.makeText(HomeActivity.this, "Access Denied", Toast.LENGTH_SHORT).show();
     }
 
     public void getAllstackOrder(){
@@ -120,7 +169,7 @@ public class HomeActivity extends AppCompatActivity {
             Log.d("CSV", "Write to CSV successful");
             File f = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Orders.csv");
             Uri attachment = Uri.fromFile(f);
-            String[] addresses = {"jainbros123@gmail.com"};
+            String[] addresses = {"abhinabera@gmail.com"};
             String subject = "JM Orders";
             composeEmail(addresses, subject, attachment);
         }
