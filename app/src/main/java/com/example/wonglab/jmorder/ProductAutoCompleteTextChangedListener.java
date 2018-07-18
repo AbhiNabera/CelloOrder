5b2cdec60570c9b3889d0aff9252057dab3d5412
AbhiNabera <abhinandannabera@yahoo.in>
2018-07-18 21:36:03 +0530

    Build Version 1.0.5

--- app/src/main/java/com/example/wonglab/jmorder/ProductAutoCompleteTextChangedListener.java
+++ app/src/main/java/com/example/wonglab/jmorder/ProductAutoCompleteTextChangedListener.java
@@ -115,7 +115,7 @@ public class ProductAutoCompleteTextChangedListener implements TextWatcher {
             //String encodedQuery = URLEncoder.encode(query, "UTF-8");
             rx.Observable<List<Element>> observable = apiInterface.getItemList(query.trim().toUpperCase());
             searchResultsSubscription = observable
-                    .debounce(100, TimeUnit.MILLISECONDS)
+                    .debounce(10, TimeUnit.MILLISECONDS)
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribeOn(Schedulers.io())
                     .subscribe(searchResultsSubscriber());
